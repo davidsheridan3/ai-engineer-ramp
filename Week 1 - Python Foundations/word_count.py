5aff50904a4a97ff20a2943cc8acf064f68f03ce
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-13 17:44:16 +0100

    Testing the use case for with blocks, and open() + read() functions.

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -4,3 +4,9 @@ def word_counter(text):
 text = input("Enter a sentence, I'll count the words: ")
 count = word_counter(text)
 print(f"Total worlds: {count}")
+
+
+with open("sample.txt", "r") as file:
+    file_content = file.read()
+
+print(file_content)
