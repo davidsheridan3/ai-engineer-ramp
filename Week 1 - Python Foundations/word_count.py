7ce5a96d4b52ac3858b670b28a1682666927dc84
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-13 14:02:30 +0100

    Refactoring: optimising by defining a word_counter function

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,7 +1,6 @@
 text = input("Hey user, enter a sentence and I'll return a word count!!!!")
-words = text.split()
-count = len(words)
 
-print(text)
-print(words)
-print("Total number of words: " + str(count))
+def word_counter(text):
+    return len(text.split())
+
+print(word_counter(text))
