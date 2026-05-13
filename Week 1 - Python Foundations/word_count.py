0d43abff34028aa64e02cdd84de2b05e7a614844
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-13 17:47:09 +0100

    Quick fix for accidental change in return component of word_counter function.

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,5 +1,5 @@
 def word_counter(text):
-    return "Total words: " + str(len(text.split()))
+    return len(text.split())
 
 text = input("Enter a sentence, I'll count the words: ")
 count = word_counter(text)
