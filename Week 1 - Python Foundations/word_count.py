8ce922ab892ec36805babdf355f3fff94edcb4ed
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-14 12:31:39 +0100

    Printing sample.txt file content in terminal. m

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,12 +1,11 @@
-def word_counter(text):
-    return len(text.split())
-
-text = input("Enter a sentence, I'll count the words: ")
-count = word_counter(text)
-print(f"Total worlds: {count}")
-
-
 with open("sample.txt", "r") as file:
     file_content = file.read()
 
 print(file_content)
+
+def word_counter(file_content):
+    return len(file_content.split())
+
+
+
+
