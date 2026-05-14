0b2621574f3ca01d6b0a5ba28fcf07b8e481d4bf
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-14 12:32:55 +0100

    Passing the file content through the word counter function (and actually displaying the word count when ran).

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,11 +1,9 @@
 with open("sample.txt", "r") as file:
     file_content = file.read()
 
-print(file_content)
-
 def word_counter(file_content):
     return len(file_content.split())
 
-
+print(word_counter(file_content))
 
 
