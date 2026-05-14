17dc64e121dcb9e7947d3f5bedb7749c7898dc39
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-14 12:46:32 +0100

    Refactoring: added some error handling as fallback. Tested it by changing the name of the txt file, but leave the code in word_count.py the same. Outputted "Error: File 'sample.txt' not found. Please check file path and try again." in terminal. Success

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,9 +1,13 @@
-with open("sample.txt", "r") as file:
-    file_content = file.read()
-
 def word_counter(file_content):
     return len(file_content.split())
 
-print(f"Total number of words in 'sample.txt' is: {word_counter(file_content)}")
+try:
+    with open("sample.txt", "r") as file:
+        file_content = file.read()
+    print(f"Total number of words in 'sample.txt' is: {word_counter(file_content)}")
+
+except FileNotFoundError:
+    print("Error: File 'sample.txt' not found. Please check file path and try again.")
+
 
 
