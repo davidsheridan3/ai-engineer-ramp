79b702574cdb47976ca08dbea09ae6d125f5a7bb
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-18 13:45:20 +0100

    Refactoring: cleaned variable names, upgraded output, grouped logic together. Console output is also much cleaner.

--- Week 1 - Python Foundations/word_count.py
+++ Week 1 - Python Foundations/word_count.py
@@ -1,39 +1,40 @@
-def word_counter(file_content):
-    return len(file_content.split())
-
-try:
-    with open("sample.txt", "r") as file:
-        file_content = file.read()
-    print(f"Total number of words in 'sample.txt' is: {word_counter(file_content)}")
-
-except FileNotFoundError:
-    print("Error: File 'sample.txt' not found. Please check file path and try again.")
-
-line_count = len(file_content.splitlines())
-print(f"Total number of lines in 'sample.txt' is: {line_count}")
-
-char_count = len(file_content)
-print(f"Total number of characters in 'sample.txt' is: {char_count}")
-
-word_list = file_content.lower().split()
-print(word_list)
-
-word_counts = {}
-for word in word_list:
-    if word not in word_counts:
-        word_counts[word] = 1
-    else:
-        word_counts[word] += 1
+def count_words(text):
+    return len(text.split())
 
 
 def get_top_words(word_counts):
     sorted_words = sorted(word_counts.items(), key=lambda item: item[1], reverse=True)
     return sorted_words[:5]
 
-top_words = get_top_words(word_counts)
 
-for word, count in top_words:
-    print(f"{word}: {count}")
+try:
+    with open("sample.txt", "r") as file:
+        text = file.read()
 
+    word_count = count_words(text)
+    line_count = len(text.splitlines())
+    char_count = len(text)
 
+    words = text.lower().split()
 
+    word_counts = {}
+
+    for word in words:
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+
+    top_words = get_top_words(word_counts)
+
+    print(f"Total number of words: {word_count}")
+    print(f"Total number of lines: {line_count}")
+    print(f"Total number of characters: {char_count}")
+
+    print("\nTop 5 most common words:")
+
+    for word, count in top_words:
+        print(f"{word}: {count}")
+
+except FileNotFoundError:
+    print("Error: File 'sample.txt' not found. Please check file path and try again.")
