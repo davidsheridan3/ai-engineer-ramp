c4c8817d694106db6eb424242a4c4809f2f79d90
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-27 22:38:54 +0100

    Data types introduction (numbers: ints and floats).

---
+++ Python/Python Basics/Data Types/main.py
@@ -0,0 +1,15 @@
+#Numbers
+# int and float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4)) # 0.5 (float)
+
+print(type(20 + 1.1)) # 21.1 (float)
+print(type(8.9 + 1.1)) # 10.0 (float, decimal stays)
+
+print(2 ** 2) # to the power of
+
+print(24 // 5) # rounded down to nearest integer
+
+print(24 % 5) # modulo, returns remainder after division
