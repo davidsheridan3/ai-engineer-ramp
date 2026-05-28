3727783363754f412d0a9323c73bf8b3b1a5bf78
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-28 15:34:58 +0100

    Variables - principles, best practices, constants.

---
+++ Python/Python Basics/variables.py
@@ -0,0 +1,19 @@
+# Variables
+# Don't use keywords to name variables
+
+user_iq = 190 # snake case
+
+print(user_iq)
+
+
+iq = 190
+user_age = iq/4
+a = user_age
+print(a)
+
+
+# Constants
+PI = 3.14 # this value shouldn't be changed
+
+d,b,c = 1,2,3 # assigning values to multiple variables at once
+print(d)
