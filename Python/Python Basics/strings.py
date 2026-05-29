9ece10d225106e9ff612fde326ca95eb2b2600ed
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-29 21:33:28 +0100

    Escape sequence principles.

--- Python/Python Basics/strings.py
+++ Python/Python Basics/strings.py
@@ -22,3 +22,18 @@ print('David' + 'Sheridan') # this runs fine!
 # Type conversion
 number = str(100)
 print('David' + number) # converting the integer 100, to string '100', allows us to add it to 'David'
+
+# Escape sequences
+weather = 'It's sunny'' # simply doesn't work, python interpreter thinks the string is being ended after t
+# so we use double quotes here
+weather = "It's sunny"
+print(weather)
+# But what about:
+weather = "It's "kind of" sunny" # breaks again
+# so here we can use escape sequences (\):
+weather_today = "It's \"kind of\" sunny"
+# '\' means next character can be assumed a string
+print(weather_today)
+# '\n' = new line
+# '\t' = tab
+
