359468317eb1b179a04d46d46b6f3e6ccbab4ee4
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-29 18:40:51 +0100

    Running through the core principles of strings.

---
+++ Python/Python Basics/strings.py
@@ -0,0 +1,16 @@
+# For strings, we can use either simgle or double quotations:
+print(type("Hello World"))
+print(type('Hello World'))
+
+# We can also use 3 single quotes in a row for LONG STRINGS:
+long_string = '''
+I went to the shop yesterday.
+It was sunny,
+so I bought an ice cream.
+'''
+print(long_string)
+
+first_name = 'David'
+surname = 'Sheridan'
+fullname = first_name + ' ' + surname
+print(fullname)
