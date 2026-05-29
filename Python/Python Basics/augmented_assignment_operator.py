1b4e9bf55150702acdd15814d8607a12d55c6b6b
davidsheridan3 <davidsheridan061@gmail.com>
2026-05-29 18:32:44 +0100

    Running through the core principle of augmented assignment operators.

---
+++ Python/Python Basics/augmented_assignment_operator.py
@@ -0,0 +1,13 @@
+some_value = 5
+# some_value = some_value + 3
+
+# print(some_value)
+
+# instead of retyping "some_value" in: some_value = some_value + 3, to append the variable we can simply use:
+some_value += 3
+print(some_value)
+
+# so we can do this for +, -, *, /
+magic_value = 5
+magic_value /= 2
+print(magic_value)
