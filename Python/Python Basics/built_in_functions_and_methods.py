94975853ece15d22f28b15fb1027df7013561633
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-01 23:34:29 +0100

    Exploring the popular built in methods

---
+++ Python/Python Basics/built_in_functions_and_methods.py
@@ -0,0 +1,8 @@
+greet = 'Helloooooo'
+print(len(greet))
+
+quote = 'to be or not to be'
+print(quote.upper()) # upper method
+print(quote.capitalize()) # capitalize method
+print(quote.find('not')) # returns index 'not' starts at, in this case: 9
+print(quote.replace('not', 'bot')) # replaces occurrence of 'not' with 'bot'
