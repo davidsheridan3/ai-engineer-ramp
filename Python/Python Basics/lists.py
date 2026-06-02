fa28bf573b1586ba20a635374918491c173e48f1
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-02 12:57:34 +0100

    Introduction to lists.

---
+++ Python/Python Basics/lists.py
@@ -0,0 +1,6 @@
+li = [1,2,3,4,5]
+li2 = ['a','b','c','d','e']
+li3 = [1,2.2,'a',True,'b','c','d','e'] # lists can contain various data types
+
+amazon_cart = ['laptop', 'computer', 'mouse', 'remote', 'keyboard']
+print(amazon_cart[0]) # like strings can access specific indexes
