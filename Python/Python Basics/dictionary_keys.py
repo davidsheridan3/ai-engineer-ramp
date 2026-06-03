f512a7509f0362f52ae5d9896780caf88841c35e
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-03 16:31:05 +0100

    Going through more useful methods for dictionaries, and also exploring a new way to create a dictionary: dict().

--- Python/Python Basics/dictionary_keys.py
+++ Python/Python Basics/dictionary_keys.py
@@ -2,9 +2,29 @@
 # 99% of the time we use something descriptive, like a string
 # Also, keys have to be unique.... if a key is repeated, previous value is overwritten
 
-dictionary = {
-    '123': [1,2,3],
-    '123': 'hello'
+# dictionary = {
+#     '123': [1,2,3],
+#     '123': 'hello'
+# }
+#
+# print(dictionary['123']) # output = 'hello'
+
+''' To access a key and to see if it even exists (to prevent getting any errors):
+lets use .get() method!!! '''
+
+user = {
+    'name': 'John',
+    'age': 25,
+    'sex': 'male',
+    'height': 70,
+    'city': 'New York'
 }
 
-print(dictionary['123']) # output = 'hello'
+# print(user['country']) # program fails! we don't want this. we want our code to runnnnn
+print(user.get('country')) # returns none, no errors :))))
+print(user.get('country', 'France')) # can also use this to set default value if key doesn't exist
+
+# another way to create dicts (dict function)
+
+user2 = dict(name='John', age=25, city='New York')
+print(user2)
