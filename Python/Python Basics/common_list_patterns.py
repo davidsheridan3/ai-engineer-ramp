8994959d29e8e464f5562173d902622b7a54397a
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-03 12:10:00 +0100

    Common list patterns: different approaches to changing order of list contents.

---
+++ Python/Python Basics/common_list_patterns.py
@@ -0,0 +1,5 @@
+letters = ['a', 'x', 'b', 'c', 'f', 'g', 'e']
+letters.sort() # puts into order
+letters.reverse() # flips the order
+print(letters[::-1]) # flips letters back again (into order)
+print(letters) # displays letters backwards
