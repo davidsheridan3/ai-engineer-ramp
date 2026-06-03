19322136229c5f286b68b5cd72c15ee931bf2fcb
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-03 12:15:56 +0100

    Common list patterns: turning a range into a list, and joining a variable with each component of a list.

--- Python/Python Basics/common_list_patterns.py
+++ Python/Python Basics/common_list_patterns.py
@@ -1,5 +1,11 @@
 letters = ['a', 'x', 'b', 'c', 'f', 'g', 'e']
 letters.sort() # puts into order
 letters.reverse() # flips the order
-print(letters[::-1]) # flips letters back again (into order)
-print(letters) # displays letters backwards
+# print(letters[::-1]) # flips letters back again (into order)
+# print(letters) # displays letters backwards
+
+print(list(range(1,101))) # list() allows us to list out all components of the range stated
+
+sentence = ' ' # space
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'David']) # joins space with each item in list, meaning we get a sentence as output
+print(new_sentence)
