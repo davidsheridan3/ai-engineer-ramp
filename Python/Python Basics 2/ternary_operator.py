3135b01ebe29744ed0174370eb1cb0a1148c21dd
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-08 13:12:03 +0100

    Python basics progress: ternary operators, truthy values vs falsey values.

---
+++ Python/Python Basics 2/ternary_operator.py
@@ -0,0 +1,6 @@
+# Ternary Operator
+
+# condtion_if_true if condition else condition_if_false
+is_friend = False
+can_message = "Allowed" if is_friend else "Not allowed"
+print(can_message) # Not allowed
