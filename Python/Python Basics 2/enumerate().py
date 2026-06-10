5dfb56ed32d7fed847e1e1c4ba49d8a54a9db344
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-10 12:33:55 +0100

    Enumerate() fundamentals

---
+++ Python/Python Basics 2/enumerate().py
@@ -0,0 +1,5 @@
+for i,char in enumerate('Hello'):
+    print(i,char) # i = index number, char = the value at that index: e.g. 0,H or 4,o
+
+for i, number in enumerate(range(100)):
+    print(f"The index of {number} is {i}")
