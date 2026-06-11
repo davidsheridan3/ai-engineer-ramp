7f3e1204e901c0074b4401a6f68a354ec76c5a63
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-11 11:12:53 +0100

    Diving further into while loops, and comparing them with for loops

--- Python/Python Basics 2/while_loops.py
+++ Python/Python Basics 2/while_loops.py
@@ -16,3 +16,12 @@ while b < 10:
     break # the break statement means we exit the while loop => the else block never gets hits
 else:
     print("Finished!")
+
+
+# while loops vs. for loops: while loops are better for an unknown loop quantity, whereas for loops are better for a known amount e.g. for items in a list
+# for while loops we need to have exit criteria to avoid infinite loops: e.g. a 'break' or an external variable
+
+while True:
+    response = input('say something: ')
+    if response == 'bye':
+        break # loop keeps running until the user inputs "bye"
