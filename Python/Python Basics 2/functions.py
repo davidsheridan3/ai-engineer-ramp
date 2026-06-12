722ef707bc0975de8a21f0a050868cc55a3aa48c
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-13 00:57:06 +0100

    Introduction to functions

---
+++ Python/Python Basics 2/functions.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("Hello")
+
+say_hello() # in order to use a function, we must call it
+
+# DRY (don't repeat yourself): functions are great for doing things over and over without repeated code
