4d82866ab5c5fc73fabd43ccf6ba535fc8e48f78
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-14 14:07:58 +0100

    Keyword arguments vs positional arguments, and default parameters

--- Python/Python Basics 2/parameters_and_arguments.py
+++ Python/Python Basics 2/parameters_and_arguments.py
@@ -6,4 +6,21 @@ def say_hello(name, age): # these parameters allow us to give the function argum
 # Arguments
 say_hello("David",24)
 
-# We can now create functions that do things based on what parameters we give it, and what arguments we call it with 
+# We can now create functions that do things based on what parameters we give it, and what arguments we call it with
+
+# positional arguments (position matters)
+say_hello("Tom",26)
+say_hello("Jerry",39)
+say_hello("William",19)
+say_hello(24,"David") # prints backwards
+
+# keyword arguments (allow us to not worry about the position)
+say_hello(age=82,name="David")
+say_hello(age=41,name="Jenny")
+
+# Default Parameters (don't confuse with keyword arguments)
+def say_goodbye(name='Tom', day='Tuesday'):
+    print(f"Goodbye {name}. I will see you again next {day}!")
+
+say_goodbye() # if no args are given, the default parameters are used : Goodbye Tom. I will see you again next Tuesday!
+say_goodbye("Aaron","Sunday") # given args overwrite the default params
