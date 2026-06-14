0ffbc18248b506eeaa08cbad8e2202b6d8954ec7
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-14 13:58:27 +0100

    Exploring how to make functions more adaptable and flexible using parameters and arguments.

---
+++ Python/Python Basics 2/parameters_and_arguments.py
@@ -0,0 +1,9 @@
+# Parameters
+def say_hello(name, age): # these parameters allow us to give the function arguments when we call it (as seen on line 7)
+    print(f"Hello {name}, you are {age} years old")
+
+
+# Arguments
+say_hello("David",24)
+
+# We can now create functions that do things based on what parameters we give it, and what arguments we call it with 
