d0cf667bff5bb3ae062a12295f44357d88e72127
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-15 12:26:43 +0100

    Differentiating methods from functions and discovering their commonalities.

---
+++ Python/Python Basics 2/methods_vs_functions.py
@@ -0,0 +1,25 @@
+# Methods vs functions
+
+'''
+Functions:
+list()
+print()
+max()
+min()
+sum()
+
+def sum(num1,num2):
+    return num1+num2
+
+sum(10,5)
+'''
+
+'''
+Methods:
+'hellooooo'.capitalize()     in this case, 'hellooooo' owns the method
+
+list = [1,2,3,4,5]
+list.pop() 
+'''
+
+# Both methods and functions allow us to take actions on our data
