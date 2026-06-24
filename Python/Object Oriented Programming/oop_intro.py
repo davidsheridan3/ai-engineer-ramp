af756fceb45c0b7da095dd1a68df9145eb27c233
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-24 14:06:09 +0100

    Using __init__ to give use control by using default arguments...

--- Python/Object Oriented Programming/oop_intro.py
+++ Python/Object Oriented Programming/oop_intro.py
@@ -13,16 +13,17 @@ obj3 = BigObject()
 class PlayerCharacter:
     # Class Object Attribute
     membership = True # static, this is going to be true and exist for all objects
-    def __init__(self, name): # self refers to PlayerCharacter
-        if self.membership:
+    def __init__(self, name = "Anonymous", age= 0): # self refers to PlayerCharacter, with default args too, for if user leaves out name or age
+        if (age > 18):
             self.name = name # self refers to player
+            self.age = age
 
     def run(self):
         print('run')
 
-player1 = PlayerCharacter('David') # object 1
-player2 = PlayerCharacter('Marcus') # object 2
-print(player1) # <__main__.PlayerCharacter object at 0x100a5ce80>
+player1 = PlayerCharacter('David',19) # object 1
+player2 = PlayerCharacter('Marcus',22) # object 2
+print(player1.age) # <__main__.PlayerCharacter object at 0x100a5ce80>
 print(player2.name) # Marcus
 
 # self allows us to have a reference to something that hasn't between created yet
