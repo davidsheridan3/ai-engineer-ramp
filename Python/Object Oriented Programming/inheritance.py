50a712cf1df5535895da6d5fb4a9355a23f9326e
davidsheridan3 <davidsheridan061@gmail.com>
2026-06-29 14:04:43 +0100

    Using the fundamentals of inheritance to share functionality across classes.

--- Python/Object Oriented Programming/inheritance.py
+++ Python/Object Oriented Programming/inheritance.py
@@ -5,10 +5,27 @@ class User:
         print('logged in')
 
 class Wizard(User): # to inherit, pass the parent class that we want to inherit from
-    pass
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with power of {self.power}')
 
 class Archer(User):
-    pass
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
 
-wizard1 = Wizard()
-print(wizard1.sign_in()) # logged in, we inherited functionality from the User class
+    def attack(self):
+        print(f'attacking with {self.num_arrows} arrows')
+
+wizard1 = Wizard('David', 100)
+archer1 = Archer('John', 50)
+wizard1.attack() # attacking with power of 100
+archer1.attack() # attacking with 50 arrows
+
+# both of these have sign in function at the same time (extracting away part of code that they both share)
+# power of inheritance ^^^^
+
+
