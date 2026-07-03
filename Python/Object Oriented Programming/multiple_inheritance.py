3379df397da9d18ae0f14624a29226c45e1e77f2
davidsheridan3 <davidsheridan061@gmail.com>
2026-07-03 12:47:28 +0100

    Workaround to allow us to access methods form multiple classes from a single instance

--- Python/Object Oriented Programming/multiple_inheritance.py
+++ Python/Object Oriented Programming/multiple_inheritance.py
@@ -22,11 +22,12 @@ class Archer(User):
         print('Running really fast!')
 
 class Hybrid(Wizard,Archer): # we want Hybrid to be able to access all methods
-    pass
+    def __init__(self, name, power,  num_arrows):
+        Wizard.__init__(self, name, power)
+        Archer.__init__(self, name, num_arrows)
 
-hybrid1 = Hybrid('Hybry',100)
+hybrid1 = Hybrid('Hybry',100, 500)
 print(hybrid1.run()) # Running really fast!
-print(hybrid1.check_arrows()) # 'Hybrid' object has no attribute 'num_arrows'
+print(hybrid1.check_arrows()) # 500 arrows remaining.
 
-# this error is happening because we inherited from Wizard first, then Archer, and Wizard accepts: name, power
 
