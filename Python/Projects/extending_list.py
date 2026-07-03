3bd2bdea8b9bcea01fb7549d73986fb8d30d5a70
davidsheridan3 <davidsheridan061@gmail.com>
2026-07-03 12:17:04 +0100

    Exercise solution

--- Python/Projects/extending_list.py
+++ Python/Projects/extending_list.py
@@ -1,10 +1,12 @@
-class SuperList():
+class SuperList(list):
     def __len__(self):
         return 1000
 
 superlist1 = SuperList()
 
 print(len(superlist1))
-superlist1.append(5) # 'SuperList' object has no attribute 'append'
+superlist1.append(100) # 'SuperList' object has no attribute 'append'
+print(superlist1[0])
 
-# we need to be able to append!
+print(issubclass(SuperList, list)) # True, SuperList is a subclass of list
+
