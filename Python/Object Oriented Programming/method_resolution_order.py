94437fc97306fe895dd677c2d10ff8e1da842438
davidsheridan3 <davidsheridan061@gmail.com>
2026-07-03 12:58:43 +0100

    Method resolution order fundamentals.

---
+++ Python/Object Oriented Programming/method_resolution_order.py
@@ -0,0 +1,25 @@
+# MRO
+
+class A:
+    num = 10
+
+class B(A):
+    pass
+
+class C(A):
+    num = 1
+
+class D(B, C):
+    pass
+
+print(D.num) # 1
+print(D.mro()) # the order of this class
+
+class X:pass
+class Y:pass
+class Z:pass
+class A(X, Y):pass
+class B(Y, Z):pass
+class M(B, A, Z):pass
+
+print(M.mro())
