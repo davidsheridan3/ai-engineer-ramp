f88758171eecf0feb3bd419152e897b885ebf460
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-10 16:49:09 +0100

    Further implementing lambda expressions to remove code not needed (filter)

--- Python/Functional Programming/lambda_expressions.py
+++ Python/Functional Programming/lambda_expressions.py
@@ -14,3 +14,9 @@ def accumulator(acc, item):
 
 print(list(map(lambda item: item * 2, my_list))) # one and done, we can just delete the multiply by 2 function now
 print(my_list)
+
+# order for lambda expressions:
+# lambda param: action(param)   call it, give it the param, then action we want to take on the param
+
+# lets remove the filter function using lambda too:
+print(list(filter(lambda item: item % 2 != 0, my_list)))
