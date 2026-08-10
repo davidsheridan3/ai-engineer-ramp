045c2a761a6774dba7810c9a918767bb76b43c08
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-10 16:51:36 +0100

    Finally replacing the accumulator function (reduce) wth lambda expression

--- Python/Functional Programming/lambda_expressions.py
+++ Python/Functional Programming/lambda_expressions.py
@@ -4,11 +4,6 @@ from functools import reduce
 
 my_list = [1,2,3]
 
-
-def accumulator(acc, item):
-    print(acc, item)
-    return acc + item
-
 print(list(map(lambda item: item * 2, my_list))) # one and done, we can just delete the multiply by 2 function now
 print(my_list)
 
@@ -17,3 +12,5 @@ print(my_list)
 
 # lets remove the filter function using lambda too:
 print(list(filter(lambda item: item % 2 != 0, my_list)))
+
+print(reduce(lambda acc, item: item + acc, my_list))
