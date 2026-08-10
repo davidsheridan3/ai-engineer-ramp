2fa344ff98a6420069d5ada5b44a4f778284aa36
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-10 16:43:52 +0100

    Exploring the power of lambda expressions

---
+++ Python/Functional Programming/lambda_expressions.py
@@ -0,0 +1,18 @@
+# lambda expressions
+
+from functools import reduce
+
+my_list = [1,2,3]
+
+def multiply_by2(item):
+    return item * 2
+
+def only_odd(item):
+    return item % 2 != 0
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+print(list(map(lambda item: item * 2, my_list))) # one and done, we can just delete the multiply by 2 function now
+print(my_list)
