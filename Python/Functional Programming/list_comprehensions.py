12574ec5bc877ec16ddef37c254a870b13fdd6ba
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-10 17:50:02 +0100

    Further implementation of list comprehensions to make code shorter (even though it does reduce readability)

--- Python/Functional Programming/list_comprehensions.py
+++ Python/Functional Programming/list_comprehensions.py
@@ -12,3 +12,12 @@ print(my_list)
 
 new_list = [char for char in 'hello']
 print(new_list) # exact same thing!!!!!
+
+new_list2 = [num for num in range(100)]
+print(new_list2)
+
+new_list3 = [num ** 2 for num in new_list2]
+print(new_list3)
+
+new_list4 = [num for num in new_list3 if num % 2 == 0]
+print(new_list4)
