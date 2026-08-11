9f051ad2066161e3ab15aa10889abb404e521394
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-11 10:00:49 +0100

    Set comprehensions: generating sets

---
+++ Python/Functional Programming/set_and_dictionary_comprehension.py
@@ -0,0 +1,17 @@
+#list, set, dictionary
+# same as list_comprehensions.py, but just sets rather than lists
+
+new_list = {char for char in 'hello'}
+print(new_list) # exact same thing!!!!!
+
+new_list2 = {num for num in range(100)}
+print(new_list2)
+
+new_list3 = {num ** 2 for num in new_list2}
+print(new_list3)
+
+new_list4 = {num for num in new_list3 if num % 2 == 0}
+print(new_list4)
+
+# quick way for us to generate sets (remember sets only allow unique items, duplicates removed
+
