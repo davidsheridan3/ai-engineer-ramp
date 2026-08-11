2faf9bbb0d9c03d439b86223eb385b4bae01b506
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-11 11:49:28 +0100

    Covering essentials of requests library

--- Building AI Web Apps/Essential Python Libraries for AI/requests_http.py
+++ Building AI Web Apps/Essential Python Libraries for AI/requests_http.py
@@ -1,5 +1,6 @@
 # The requests library (HTTP for Humans)
 import requests
+import httpx
 
 url = 'https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m'
 response = requests.get(url)
@@ -9,3 +10,9 @@ print(response.headers)
 print(response.text)
 data = response.json()
 print(data['hourly']['temperature_2m']) # temperature values as a list
+
+r = httpx.get(url)
+print(r.status_code)
+print(r.json())
+
+# httpx mirror the simplicity of requests, while adding additional capabilities such as asynchronous requests, built in connection pooling, and http 2 support
