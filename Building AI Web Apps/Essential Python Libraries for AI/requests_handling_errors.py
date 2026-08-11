530a5c58f1f39c192b0460c65f6e178e3edec4d5
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-11 23:51:27 +0100

    Editing the url to a fake one to produce and error (404 as expected)

--- Building AI Web Apps/Essential Python Libraries for AI/requests_handling_errors.py
+++ Building AI Web Apps/Essential Python Libraries for AI/requests_handling_errors.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 try:
-    url = 'https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m'
+    url = 'https://api.open-meteo.com/st?latitude=35&longitude=139&hou'
     response = requests.get(url, timeout=5) # ensures the request will only wait 5 seconds before timing out
     response.raise_for_status()
     print(json.dumps(response.json(), indent=4))
