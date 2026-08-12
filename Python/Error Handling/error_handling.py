b38ab0dcef054dd1cf3dc9ac147313d02cefb854
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-12 23:32:53 +0100

    Found that the program just ends when except block is hit, so to allow the user to re-enter an age, added a while loop, loop also now breaks when user inputs valid age

--- Python/Error Handling/error_handling.py
+++ Python/Error Handling/error_handling.py
@@ -11,9 +11,14 @@
 # now when a string is entered the user receives an error: ValueError: invalid literal for int() with base 10
 
 # we can catch the error in an except block, and prompt the user further:
-try:
-    age = int(input('Enter your age: '))
-    print('Your age is', age)
-except:
-    print('Please enter a number!')
+
+while True:
+    try:
+        age = int(input('Enter your age: '))
+        print('Your age is', age)
+    except:
+        print('Please enter a number!')
+    else:
+        print('Thank you for your time!')
+        break
 
