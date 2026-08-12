c5a50eb3a9716d1bfeef14e5cd4bf4a7760ad9cf
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-12 23:27:42 +0100

    Implementing a 'try except' block for error handling

--- Python/Error Handling/error_handling.py
+++ Python/Error Handling/error_handling.py
@@ -5,7 +5,15 @@
 # allows user to enter a string, we don't want this
 
 # so we use int():
-age = int(input('Enter your age: '))
-print('Your age is', age)
+# age = int(input('Enter your age: '))
+# print('Your age is', age)
 
 # now when a string is entered the user receives an error: ValueError: invalid literal for int() with base 10
+
+# we can catch the error in an except block, and prompt the user further:
+try:
+    age = int(input('Enter your age: '))
+    print('Your age is', age)
+except:
+    print('Please enter a number!')
+
