14dfa2eb033f20236fe8f9c03c1f0d123c3300b4
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-12 09:53:50 +0100

    Realising limitations of adding params

--- Python/Decorators/decorators2.py
+++ Python/Decorators/decorators2.py
@@ -2,17 +2,18 @@
 # A function that wraps another function and enhances it or changes it
 
 def my_decorator(func):
-    def wrap_func(greeting):
+    def wrap_func(greeting, emoji):
         print("********")
-        func(greeting)
+        func(greeting, emoji)
         print("********")
     return wrap_func
 
 # what if hello() actually takes a parameter
+# if we add more params to hello(), we need to add them to wrap_func(), this gets tedious!
 
 @my_decorator
-def hello(greeting):
-    print(greeting)
+def hello(greeting, emoji):
+    print(greeting, emoji)
 
-hello('hiiii') # now output = hiiii, as added parameter to wrap_func
+hello('hiiii', ':(')
 
