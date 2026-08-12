1fd67fb49898d2cb977387f959e7c10764a7ddcd
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-12 23:24:03 +0100

    Building for this edge case

--- Python/Error Handling/error_handling.py
+++ Python/Error Handling/error_handling.py
@@ -1,5 +1,11 @@
 # Error Handling
-age = input('Enter your age: ')
-print('Your age is', age)
+# age = input('Enter your age: ')
+# print('Your age is', age)
 
 # allows user to enter a string, we don't want this
+
+# so we use int():
+age = int(input('Enter your age: '))
+print('Your age is', age)
+
+# now when a string is entered the user receives an error: ValueError: invalid literal for int() with base 10
