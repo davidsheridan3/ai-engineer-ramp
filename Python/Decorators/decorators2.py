fc6570c2ddf722ae18abeb6e3154bd403428e679
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-12 09:58:07 +0100

    Implememting solution to extending params (*args, **kwargs)

--- Python/Decorators/decorators2.py
+++ Python/Decorators/decorators2.py
@@ -2,18 +2,19 @@
 # A function that wraps another function and enhances it or changes it
 
 def my_decorator(func):
-    def wrap_func(greeting, emoji):
+    def wrap_func(*args, **kwargs):
         print("********")
-        func(greeting, emoji)
+        func(*args, **kwargs)
         print("********")
     return wrap_func
 
 # what if hello() actually takes a parameter
 # if we add more params to hello(), we need to add them to wrap_func(), this gets tedious!
+# solution for this is '*args, **kwargs' for params in wrap func accepts all params and key:value params
 
 @my_decorator
-def hello(greeting, emoji):
+def hello(greeting, emoji=':('):
     print(greeting, emoji)
 
-hello('hiiii', ':(')
+hello('hiiii')
 
