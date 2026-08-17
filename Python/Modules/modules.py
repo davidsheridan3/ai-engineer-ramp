6563843efffba0fcbe0c35ffccf4ec8d612f2866
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-17 10:57:01 +0100

    Exploring modules and what they actually mean + importing a utility module into a main.py file and using it's functions

---
+++ Python/Modules/modules.py
@@ -0,0 +1,7 @@
+# this 'modules.py' is a module. We want to separate modules for separate purposes.
+import utility
+
+print(utility.divide(10,2))
+print(utility.multiply(10,2))
+
+# using utility module (utility.py) in my main file!
