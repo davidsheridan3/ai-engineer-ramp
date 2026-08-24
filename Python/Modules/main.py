e37d8c4ef6b4f7e29ee4d459ee40c5fe52c034b4
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-24 10:16:15 +0100

    More work with modules + packages, and using them in main.py (shopping cart)

--- Python/Modules/main.py
+++ Python/Modules/main.py
@@ -1,7 +1,9 @@
 # this 'modules.py' is a module. We want to separate modules for separate purposes.
 import utility
+import Shopping.shopping_cart as shopping_cart
 
 print(utility.divide(10,2))
 print(utility.multiply(10,2))
+print(shopping_cart.buy('apple'))
 
 # using utility module (utility.py) in my main file!
