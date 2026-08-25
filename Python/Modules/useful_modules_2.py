62bb0488f55b087855bfe6ac53e58f1ec096cd3f
davidsheridan3 <davidsheridan061@gmail.com>
2026-08-25 19:54:37 +0100

    Exploring datetime and array packages (more useful modules)

---
+++ Python/Modules/useful_modules_2.py
@@ -0,0 +1,11 @@
+import datetime
+from array import array
+
+print(datetime.time(5,45,2)) # 05:45:02, allows us to create time objects
+print(datetime.date.today()) # 2026-08-25, today's date
+
+arr = array('i', [1,2,3,4,5])
+
+print(arr[2]) # 3
+
+
