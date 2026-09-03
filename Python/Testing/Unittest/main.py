75cf93f77be9b2b8d245af077171b5f2efa61322
davidsheridan3 <davidsheridan061@gmail.com>
2026-09-03 09:24:41 +0100

    Building my first unit test

---
+++ Python/Testing/Unittest/main.py
@@ -0,0 +1,2 @@
+def do_something(num):
+    return num + 5
---
+++ Python/Testing/Unittest/test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import do_something
+
+class Test(unittest.TestCase):
+    def test_do_something(self):
+        test_param = 10
+        result = do_something(test_param)
+        self.assertEqual(result, 15)
+
+unittest.main()
+
+# Ran 1 test in 0.000s
+#
+# OK
+
+# when assertEqual param = 10, we get an error as 15 is expected result from test_param of 10
+# AssertionError: 15 != 10
+#
+# ----------------------------------------------------------------------
+# Ran 1 test in 0.000s
+#
+# FAILED (failures=1)
