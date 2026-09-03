e53a5ec5e2d689a97a95071dfbf585a46bdca818
davidsheridan3 <davidsheridan061@gmail.com>
2026-09-03 09:38:29 +0100

    Adding an extra function test for None parameter. Improved function so that it's more durable, all from using tests!

--- Python/Testing/Unittest/main.py
+++ Python/Testing/Unittest/main.py
@@ -1,5 +1,8 @@
 def do_something(num):
     try:
-        return int(num) + 5
+        if num:
+            return int(num) + 5
+        else:
+            return 'please enter a number'
     except ValueError as err:
         return err
--- Python/Testing/Unittest/test.py
+++ Python/Testing/Unittest/test.py
@@ -1,4 +1,7 @@
 import unittest
+
+from pygame.macosx import Video_AutoInit
+
 from main import do_something
 
 class Test(unittest.TestCase):
@@ -10,18 +13,11 @@ class Test(unittest.TestCase):
     def test_do_something_2(self):
         test_param = 'davdhssjsns'
         result = do_something(test_param)
-        self.assertEqual(isinstance(result, ValueError), True)
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_something_3(self):
+        test_param = None
+        result = do_something(test_param)
+        self.assertEqual(result, 'please enter a number')
 
 unittest.main()
-
-# Ran 1 test in 0.000s
-#
-# OK
-
-# when assertEqual param = 10, we get an error as 15 is expected result from test_param of 10
-# AssertionError: 15 != 10
-#
-# ----------------------------------------------------------------------
-# Ran 1 test in 0.000s
-#
-# FAILED (failures=1)
